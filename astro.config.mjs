aee17de69424af1473cc5cc0c679f493de256278
Jerry Bruce <jbruc76@wgu.edu>
2025-10-27 12:19:33 -0500

    cleaned up preview options

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jerrybruce.github.io',
-  base: '/integrity-commercial/',
   integrations: [react()],
 });
