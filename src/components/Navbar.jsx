69b5e478a47b28fa9fe903109013f6c584c1d9e7
JerryBruce <jerry.r.bruce@gmail.com>
2026-01-26 12:03:05 -0600

    fixed navbar issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Navbar() {
+    // 1. Create a state to track if the menu is open
+    const [isOpen, setIsOpen] = useState(false);
 
-	return (
-		<>
-			<nav className="navbar navbar-inverse common-menu" id="top-nav">
-				<div className="container navigation-tb">
-					<div className="navbar-header">
-						<button data-toggle="collapse-side" data-target=".side-collapse" data-target-2=".side-collapse-container" type="button" className="navbar-toggle pull-left nav-icon1"><span className="icon-bar"></span><span className="icon-bar"></span><span className="icon-bar"></span></button>
-					</div>
-					<div className="collapse navbar-collapse no-padding navbar-inverse res-menu side-collapse in">
-						<div id="menu-list">
-							<div className="menu-home-width no-padding">
-								<ul className="nav navbar-nav navbar-left no-margin accordion" id="accordion">
-									<li className="close-icon"><p className="close-btn"><i className="fa fa-times" aria-hidden="true"></i></p></li>
-									<li className="dropdown menu-fs menu">
-										<a href="/" className="pagescroll menu-link dropdown-toggle" data-toggle="dropdown">Home </a>
-									</li>
-									<li className="dropdown menu-fs menu"><a href="/about/" className="pagescroll dropdown-toggle"> About</a></li>
-									<li className="dropdown menu-fs menu"><a href="/team/" className="pagescroll dropdown-toggle"> Our Team</a></li>
-									<li className="dropdown menu-fs menu"><a href="/contact/" className="pagescroll dropdown-toggle"> contact</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div >
-			</nav >
-		</>
-	)
+    // 2. Function to toggle that state
+    const toggleMenu = () => setIsOpen(!isOpen);
+
+    return (
+        <>
+            <nav className="navbar navbar-inverse common-menu" id="top-nav">
+                <div className="container navigation-tb">
+                    <div className="navbar-header">
+                        {/* 3. Added onClick and kept your exact classes */}
+                        <button 
+                            type="button" 
+                            className="navbar-toggle pull-left nav-icon1"
+                            onClick={toggleMenu}
+                        >
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                    </div>
+
+                    {/* 4. The "in" class is now controlled by React state */}
+                    <div className={`collapse navbar-collapse no-padding navbar-inverse res-menu side-collapse ${isOpen ? 'in' : ''}`}>
+                        <div id="menu-list">
+                            <div className="menu-home-width no-padding">
+                                <ul className="nav navbar-nav navbar-left no-margin accordion" id="accordion">
+                                    {/* 5. Added onClick to close button */}
+                                    <li className="close-icon" onClick={() => setIsOpen(false)}>
+                                        <p className="close-btn">
+                                            <i className="fa fa-times" aria-hidden="true"></i>
+                                        </p>
+                                    </li>
+                                    <li className="dropdown menu-fs menu">
+                                        <a href="/" className="pagescroll menu-link dropdown-toggle">Home </a>
+                                    </li>
+                                    <li className="dropdown menu-fs menu">
+                                        <a href="/about/" className="pagescroll dropdown-toggle"> About</a>
+                                    </li>
+                                    <li className="dropdown menu-fs menu">
+                                        <a href="/team/" className="pagescroll dropdown-toggle"> Our Team</a>
+                                    </li>
+                                    <li className="dropdown menu-fs menu">
+                                        <a href="/contact/" className="pagescroll dropdown-toggle"> contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
 }
 
 export default Navbar;
