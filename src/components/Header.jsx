df253ed23793a2d2fc62d5bf3a1bf61af81995c6
JerryBruce <jerry.r.bruce@gmail.com>
2026-01-26 12:22:38 -0600

    set up header to link back to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,10 @@ function Header() {
             <div className="container">
                 <div className="row">
                     <div className="col-md-10 column-center header-menu ">
-                        <div style={{padding: 50}}>
-                            <img src="/images/Banner.jpg" width={800}/>
+                        <div style={{ padding: 50 }}>
+                            <a href="/">
+                                <img src="/images/Banner.jpg" width={800} />
+                            </a>
                             <div className="col-md-10 column-center no-padding">
                                 <p className="center ls btn-top">We build trust by delivering every job, large or small, with integrity, respect, responsibility, and an unmatched passion for helping our partnes succeed.</p>
                             </div>
